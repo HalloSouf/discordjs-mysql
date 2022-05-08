f11a7ab4b1d21caf17c77c53ec6bf0dd18fccd0a
Soufiane <soufianesedd@gmail.com>
2022-05-08 22:34:50 +0200

    Setup prisma-client, added database-guild synchronization

--- app.js
+++ app.js
@@ -4,15 +4,32 @@
  * @author HalloSouf
  * @version 2.0.0
  */
-
 const { Client, Intents } = require('discord.js');
 const { config } = require('dotenv');
+const { PrismaClient } = require('@prisma/client');
 config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES] });
+const prisma = new PrismaClient();
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
-    console.log(`| ${client.user.tag} (${client.user.id}) is ready for use!`)
+    console.log(`| ${client.user.tag} (${client.user.id}) is ready for use!`);
+
+    client.guilds.cache.map(async (guild) => {
+        let fetchedGuild = await prisma.guild.findFirst({ 
+            where: { id: guild.id }
+        });
+
+        if (!fetchedGuild) {
+            try {
+                await prisma.guild.create({
+                    data: { id: guild.id, owner: guild.ownerId }
+                });
+            } catch (e) {
+                console.error('Something went wrong with creating guild-record: ', e.message);
+            }
+        }
+    });
 });
 
 client
